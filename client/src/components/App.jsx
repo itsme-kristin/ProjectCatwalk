7c1d53f1b27de528836235655e5686132ee0b203
bkern98 <bkern288@gmail.com>
2021-09-10 12:35:47 -0500

    Set up App component to get a product from the api

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  return <h1>App</h1>;
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get('/api/products')
+      .then(products => {
+        setProduct(products.data[0]);
+      });
+  }, []);
+
+  if (product) {
+    return <h1>{product.name}</h1>;
+  }
+  else {
+    return <h1>Hi</h1>
+  }
 };
 
 export default App;
