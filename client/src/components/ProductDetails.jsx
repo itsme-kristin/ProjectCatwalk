a637428cbfc49caaf5171f16f6e1f54a7eb2a031
Donald Vida <donald.vida@gmail.com>
2021-09-12 20:59:57 -0500

    Converted ProductDetails Component to use material_ui components.

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -1,71 +1,76 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Rating from '@material-ui/lab/Rating';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles({
+  root: {
+    border: "dotted 1px grey",
+    padding: "20px"
+  },
+
+  category: {
+    fontWeight: "700"
+  },
+
+  reviewsLink: {
+    marginLeft: "10px",
+    textDecoration: "underline"
+  },
+
+  temp: {
+    minHeight: "200px",
+    border: "dotted 1px grey",
+    marginBottom: "20px"
+  }
+})
+
 const ProductDetails = () => {
-  // TEMPORARY STYLING
-  var styles = {
-    flexGrow: "1",
-    border: "solid 1px black",
-    padding: "15px",
-    textAlign: "left",
-  }
-
-  var styles2 = {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-evenly",
-    padding: "10px",
-  }
-
-  var styles3 = {
-    width: "50px",
-    height: "50px",
-    border: "solid 1px black",
-    borderRadius: "25px",
-    textAlign: "center",
-  }
+  const classes = useStyles();
 
   return (
-    <div style={styles}>
-      <p>Star Rating (read all reviews)</p>
-      <h3>Category</h3>
-      <h1>Product Name</h1>
-      <p>Price</p>
-      <div>
-        <div><h3><span>Style ></span> Selected Style</h3></div>
-        <div>
-          <div style={styles2}>
-            <div style={styles3}>S</div>
-            <div style={styles3}>S</div>
-            <div style={styles3}>S</div>
-            <div style={styles3}>S</div>
-          </div>
-          <div style={styles2}>
-            <div style={styles3}>S</div>
-            <div style={styles3}>S</div>
-            <div style={styles3}>S</div>
-            <div style={styles3}>S</div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div>
-          <TextField variant="outlined" defaultValue="Select Size"></TextField>
+    <Grid container className={classes.root} direction="column" justifyContent="flex-start" alignItems="stretch">
+      <Grid container direction="row" alignItems="center">
+        <Rating name="avgProductRating" value={3.7}  precision={0.25} readOnly />
+        <Typography variant="caption" className={classes.reviewsLink}>
+           Read all # reviews
+        </Typography>
+      </Grid>
+      <Typography variant="h6">
+        Category
+      </Typography>
+      <Typography className={classes.category} variant="h3" gutterBottom>
+        Product Name
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        $369.00
+      </Typography>
+      <Grid container alignItems="center" className={classes.temp} justifyContent="center">
+        <Typography variant="overline">
+          Styles Select Component
+        </Typography>
+      </Grid>
+      <Grid container direction="row" justifyContent="flex-start" spacing={1}>
+        <Grid item xs={8}>
+          <TextField variant="outlined" defaultValue="Select Size" fullWidth></TextField>
+        </Grid>
+        <Grid item xs={2}>
           <Select variant="outlined" label="Qty">
             <MenuItem>1</MenuItem>
           </Select>
-        </div>
-        <div>
+        </Grid>
+        <Grid item xs={12}>
           <Button variant="contained" color="primary">Add to Cart</Button>
-        </div>
-      </div>
-    </div>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
