a50b3f0635b69ac78e17405a7922f31168761d22
Kristin Gadgil <kristingadgil@gmail.com>
2021-09-14 20:39:09 -0500

    Fixed map function in YourOutfit

--- client/src/components/YourOutfit/YourOutfit.jsx
+++ client/src/components/YourOutfit/YourOutfit.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import AddOutfitButton from './AddOutfitButton.jsx';
-import ProductCard from '../RelatedProducts/ProductCard.jsx';
+import OutfitCard from './OutfitCard.jsx';
 
 const YourOutfit = (props) => {
-  // const [outfitList, setOutfitList] = useState[[]];
-  // const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const [outfitList, setOutfitList] = useState([]);
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const chevronWidth = 40;
 
   return (
     <React.Fragment>
       <h3>YOUR OUTFIT</h3>
-       <AddOutfitButton currentProduct={props.currentProduct} />
+      <div style={{ padding: `0 ${chevronWidth}px` }}>
+        <ItemsCarousel
+          requestToChangeActive={setActiveItemIndex}
+          activeItemIndex={activeItemIndex}
+          numberOfCards={4}
+          gutter={20}
+          leftChevron={<button>{'<'}</button>}
+          rightChevron={<button>{'>'}</button>}
+          outsideChevron
+          chevronWidth={chevronWidth}
+        >
+          <AddOutfitButton currentProduct={props.currentProduct} />
+          {outfitList ? outfitList.map((product, index) => { return <OutfitCard productID={product} key={index} />}) : null
+          }
+        </ItemsCarousel>
+      </div>
     </React.Fragment>
   )
 };
