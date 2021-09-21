a93f2823f9c37290fe7069992881804e2bc4b8df
bkern98 <bkern288@gmail.com>
2021-09-21 15:19:28 -0500

    Added thumbnails to reviews that have photos. The photos expand into a modal when clicked

---
+++ client/src/components/RatingsReviews/ReviewPhoto.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Grid, Paper, Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  modal: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  thumb: {
+    margin: '4px',
+    width: '50px',
+    height: '50px'
+  }
+});
+
+const ReviewPhoto = ({ photo }) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  return (
+    <React.Fragment>
+      <Grid item xs={2}>
+        <Paper
+          className={classes.thumb}
+          onClick={() => setOpen(true)}
+          style={{
+            backgroundImage: `url(${photo.url})`,
+            backgroundSize: '100% 100%',
+            backgroundRepeat: 'no-repeat'
+          }}
+          elevation={3}
+        ></Paper>
+      </Grid>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <Grid className={classes.modal}>
+          <img src={photo.url} />
+        </Grid>
+      </Modal>
+    </React.Fragment>
+  );
+};
+
+export default ReviewPhoto;
