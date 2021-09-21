743fff4f6b47ccdf94d6c6c51bedd3311dd13ea0
Kristin Gadgil <kristingadgil@gmail.com>
2021-09-21 11:20:08 -0500

    Fixed delete outfit bug

--- client/src/components/YourOutfit/YourOutfit.jsx
+++ client/src/components/YourOutfit/YourOutfit.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ItemsCarousel from 'react-items-carousel';
 import AddOutfitButton from './AddOutfitButton.jsx';
 import OutfitCard from './OutfitCard.jsx';
@@ -14,8 +14,14 @@ const useStyles = makeStyles((theme) => ({
 
 const YourOutfit = ({ currentProduct, outfitList, setOutfitList }) => {
   const classes = useStyles();
+  const [isDeleting, setIsDeleting] = useState(false);
   const [activeItemIndex, setActiveItemIndex] = useState(0);
   const chevronWidth = 40;
+
+  useEffect(() => {
+    setIsDeleting(true);
+    setOutfitList(outfitList);
+  }, [outfitList]);
 
   const handleAddOutfitClick = () => {
     if (outfitList.length === 0) {
@@ -34,6 +40,7 @@ const YourOutfit = ({ currentProduct, outfitList, setOutfitList }) => {
   }
 
   const handleDeleteOutfitClick = (id) => {
+    setIsDeleting(false);
     const editedOutfitList = outfitList.slice();
     for (var i = 0; i < editedOutfitList.length; i++) {
       var current = editedOutfitList[i];
@@ -44,39 +51,42 @@ const YourOutfit = ({ currentProduct, outfitList, setOutfitList }) => {
       }
     }
   }
-
-  return (
-    <React.Fragment>
-      <Typography className={classes.title} variant="h4" gutterBottom>
-        YOUR OUTFIT
-      </Typography>
-      <Grid container direction="row">
-        <Grid item xs={2}>
-          <AddOutfitButton handleAddOutfitClick={handleAddOutfitClick}/>
+  if (outfitList && isDeleting) {
+    return (
+      <React.Fragment>
+        <Typography className={classes.title} variant="h4" gutterBottom>
+          YOUR OUTFIT
+        </Typography>
+        <Grid container direction="row">
+          <Grid item xs={2}>
+            <AddOutfitButton handleAddOutfitClick={handleAddOutfitClick}/>
+          </Grid>
+          <Grid item xs={10}>
+            <div style={{ padding: `0 ${chevronWidth}px` }}>
+              <ItemsCarousel
+                requestToChangeActive={setActiveItemIndex}
+                activeItemIndex={activeItemIndex}
+                numberOfCards={3}
+                gutter={20}
+                leftChevron={<button>{'<'}</button>}
+                rightChevron={<button>{'>'}</button>}
+                outsideChevron
+                chevronWidth={chevronWidth}
+              >
+                {outfitList.map((product, index) => <OutfitCard
+                  handleDeleteOutfitClick={handleDeleteOutfitClick}
+                  product={product}
+                  key={index}
+                />)}
+              </ItemsCarousel>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={10}>
-          <div style={{ padding: `0 ${chevronWidth}px` }}>
-            <ItemsCarousel
-              requestToChangeActive={setActiveItemIndex}
-              activeItemIndex={activeItemIndex}
-              numberOfCards={3}
-              gutter={20}
-              leftChevron={<button>{'<'}</button>}
-              rightChevron={<button>{'>'}</button>}
-              outsideChevron
-              chevronWidth={chevronWidth}
-            >
-              {outfitList.map((product, index) => <OutfitCard
-                handleDeleteOutfitClick={handleDeleteOutfitClick}
-                product={product}
-                key={index}
-              />)}
-            </ItemsCarousel>
-          </div>
-        </Grid>
-      </Grid>
-    </React.Fragment>
-  )
+      </React.Fragment>
+    )
+  } else {
+    return null;
+  }
 };
 
 export default YourOutfit;
