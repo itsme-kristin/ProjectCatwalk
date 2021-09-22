aee250d0672ea80895c7d3a5b1eecc87301faf5d
bkern98 <bkern288@gmail.com>
2021-09-22 15:21:59 -0500

    Rating validation

---
+++ client/src/components/RatingsReviews/Form/FormRating.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { FormControl, FormLabel, FormHelperText } from '@material-ui/core';
+import { Rating } from '@material-ui/lab';
+
+const FormRating = ({ rating, handleFormChange }) => {
+  const [error, setError] = useState('');
+
+  const handleChange = (value) => {
+    setError('')
+    if (!rating) {
+      setError('Select a Rating');
+    }
+    handleFormChange('rating', value)
+  }
+
+  const helperText = error.length > 0 ? (
+    <FormHelperText error>{error}</FormHelperText>
+  ) : null;
+
+  return (
+    <FormControl component='fieldset' required>
+      <FormLabel component='legend'>How do you rate this product?</FormLabel>
+      {helperText}
+      <Rating
+        name='overall-rating'
+        value={rating}
+        onChange={(e, newValue) => handleChange(newValue)}
+      />
+    </FormControl>
+  );
+};
+
+export default FormRating;
