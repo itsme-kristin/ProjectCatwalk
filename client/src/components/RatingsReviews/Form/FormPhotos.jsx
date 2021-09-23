792ccfec82512eac5019ff21d71e4f5b3168ec12
bkern98 <bkern288@gmail.com>
2021-09-23 11:24:12 -0500

    Forgot to track this file earlier

---
+++ client/src/components/RatingsReviews/Form/FormPhotos.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FormControl, FormLabel } from '@material-ui/core';
+import UploadPhotos from '../UploadPhotos.jsx';
+
+const FormPhotos = ({ field, form }) => {
+  return (
+    <FormControl component='fieldset'>
+      <FormLabel component='legend'>Photos</FormLabel>
+      <UploadPhotos setFieldValue={form.setFieldValue} />
+    </FormControl>
+  );
+};
+
+export default FormPhotos;
